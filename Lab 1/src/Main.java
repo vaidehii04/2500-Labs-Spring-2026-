9f96cbfc1ed45ddd00b94e7b24714ac493e6b155
vaidehii04 <vaidehip3445@gmail.com>
2026-01-24 17:43:39 -0500

    initial commit

---
+++ Lab 1/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Main {
+public static LinkedList CMult(LinkedList L1) {
+	
+}
+public static boolean inSubstack(LinkedList a, LinkedList b) {
+	
+}
+
+public static void main(String[] args) {
+	//To do the problem
+	//problem 1
+	product = 1;
+	
+	//problem2
+	
+	//problem3 
+	//call method subset
+}
+}
