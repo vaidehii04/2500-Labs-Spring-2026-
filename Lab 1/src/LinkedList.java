53be36787bbd3867eda8ef8d136502df771a4540
vaidehii04 <vaidehip3445@gmail.com>
2026-02-01 19:41:17 -0500

    initial commit

--- Lab 1/src/LinkedList.java
+++ Lab 1/src/LinkedList.java
@@ -1,4 +1,44 @@
-
 public class LinkedList {
+    Node head;
+    // Creating a constructor where head = null
+    public LinkedList(){
+        head = null;
+    }
+    public void add (int data){
+        Node newNode = new Node(data);
+        if (head ==null){
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null){
+            current = current.next;
 
+        }
+        current.next = newNode;
+    }
+    public boolean contains (int value){
+        Node current = head;
+        while (current != null){
+            if (current.data == value){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+@Override
+public String toString(){
+    String result = "[";
+    Node current = head;
+    while (current != null) {
+        result += current.data;
+        if (current.next != null){
+            result += ", ";
+        }
+        current = current.next;
+    }
+    result += "]";
+    return result;
+    }
 }
--- Lab 1/src/Main.java
+++ Lab 1/src/Main.java
@@ -1,22 +1,37 @@
-import java.util.Scanner;
-
-
+import.java.util.Scanner;
 public class Main {
-public static LinkedList CMult(LinkedList L1) {
-	
-}
-public static boolean inSubstack(LinkedList a, LinkedList b) {
-	
-}
+	public static LinkedList CMult(LinkedList L1) {
+		LinkedList L2 = new LinkedList();
+		int product = 1;
+		Node current = L1.head;
+		while (current != null) {
+			product *= current.data;
+			L2.add(product);
+			current = current.next;
+		}
+		return L2;
 
-public static void main(String[] args) {
-	//To do the problem
-	//problem 1
-	product = 1;
-	
-	//problem2
-	
-	//problem3 
-	//call method subset
-}
+	}
+	public static LinkedList mergeAlternate(LinkedList a, LinkedList b){
+		LinkedList result = new LinkedList();
+		Node p1 = a.head;
+		Node p2 = b.head;
+		while (p1 != null && p2 != null) {
+			result.add(p1.data);
+			result.add(p2.data);
+			p1 = p1.next;
+			p2 = p2.next;	
+		}
+		while (p1 != null) {
+			result.add(p1.data);
+			p1 = p1.next;
+		}
+		while (p2 != null) {
+			result.add(p2.data);
+			p2 = p2.next;
+		}
+
+
+		return result;
+	}
 }
