2784c79b52359c196331d01abca514cff47931ef
vaidehii04 <vaidehip3445@gmail.com>
2026-02-12 14:34:58 -0500

    Add lab 2 and all that

---
+++ Lab 2/Lab2/src/Queue.java
@@ -0,0 +1,16 @@
+public class Queue {
+
+    public int Dequeue(){
+        if (head == tail){
+            return null;
+        }
+        else {
+            System.out.println("The queue is empty ")
+        }
+        
+
+
+}
+}
+
+
